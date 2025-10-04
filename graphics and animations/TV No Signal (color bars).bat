f47613ca0494dcd76385aa113af18f6109ef67aa
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-10-04 03:22:29 +0100

    Graphics and animations made in Notepad.

---
+++ graphics and animations/TV No Signal (color bars).bat
@@ -0,0 +1,38 @@
+:: Created by Sebastian Januchowski
+:: polsoft.its@fastservice.com
+:: TV no signal color bars
+@echo OFF
+mode con cols=80 lines=25
+color f
+cls
+echo    [2mCreated by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m 
+echo.
+echo.
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m          polsoft.ITS          [47m                       
+echo.[43m                             [41m                   [43m                             
+echo.[0m
+pause >nul
+
+
