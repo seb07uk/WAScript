da26d7014a5d704bfdcd0545523e8d271f3f1e76
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-10-04 03:48:20 +0100

    Network management and support tools...

---
+++ network/CMD FTP.bat
@@ -0,0 +1,8 @@
+:: Created by Sebastian Januchowski
+:: polsoft.ITS London
+:: polsoft.its@fastservice.com
+@echo off
+Title CMD FTP
+echo    Created by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com
+echo.
+ftp
