d21a757db5761327910a34e13ce6a523a54dbcbd
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-10-25 17:46:07 +0100

    Update Multi-Tools CleanUp.bat

--- _UPDATE_/Multi-Tools CleanUp.bat
+++ _UPDATE_/Multi-Tools CleanUp.bat
@@ -6,8 +6,7 @@
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 Title Multi-Tools CleanUp by seb07uk
 :MENU
-echo   [2;3mCreated by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m
-echo.
+echo   [2;3mCreated by Sebastian Januchowski
 ECHO.
 echo    [1;3;34mMulti-Tools CleanUp[0m
 ECHO.
