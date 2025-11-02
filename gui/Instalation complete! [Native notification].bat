191d82bce92f3e26611f2e433c666bdfd4fe96f9
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-11-02 10:09:06 +0000

    Native notification

---
+++ gui/Instalation complete! [Native notification].bat
@@ -0,0 +1,3 @@
+@echo off 
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Installation complete!', 'polsoft.ITS London', [System.Windows.Forms.ToolTipIcon]::None)}"
--- gui/Native Windows Notifications.bat
+++
@@ -1,6 +0,0 @@
-:: Created by Sebastian Januchowski
-:: polsoft.its@fastservice.com
-
-@echo off 
-
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Hello world', 'This is called from a batch script.', [System.Windows.Forms.ToolTipIcon]::None)}"
