b8057c2f094198221005492d59b7730afd98d110
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-11-02 06:32:18 +0000

    Python + add-ons

---
+++ install and update/Python/Python install manager 25.0 WinGet Installer.bat
@@ -0,0 +1,13 @@
+:: Created by Sebastian Januchowski
+:: polsoft.its@fastservice.com
+:: https://github.com/seb07uk
+@echo off
+title Python install manager 25.0 WinGet Installer
+echo   [2;3mCreated by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m
+echo.
+echo [34m--- Python install manager 25.0 via WinGet ---[0m
+echo.[33m
+winget install 9NQ7512CXL7T
+echo.[0m
+echo [32m--- Installation complete! ---[0m
+timeout /t 5 >nul
