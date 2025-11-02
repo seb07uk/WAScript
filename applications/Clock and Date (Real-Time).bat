9bb328d114826a2f3434877425fe7406cc2aba6d
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-11-02 05:50:32 +0000

    Simple clock and date (Real-Time)

---
+++ applications/Clock and Date (Real-Time).bat
@@ -0,0 +1,10 @@
+@echo off
+:display
+cls
+echo    [2;3mWritten by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m
+echo.
+echo Current Date and Time:
+echo     %date%
+echo        %time%
+timeout /t 1 >nul
+goto display
