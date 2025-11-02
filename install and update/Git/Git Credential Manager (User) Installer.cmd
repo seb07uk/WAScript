d1a319a34507d716800d5136ea9d8e5604e83c19
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-11-02 07:50:38 +0000

    Git software
    Git programs and applications.

---
+++ install and update/Git/Git Credential Manager (User) Installer.cmd
@@ -0,0 +1,16 @@
+:: Created by Sebastian Januchowski
+:: polsoft.its@fastservice.com
+:: https://github.com/seb07uk
+@echo off
+title Git Credential Manager (User) Installer via WinGet
+
+	echo    [3;2mWritten by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m
+echo. 
+echo [32m*** Downloading and installing Git Credential Manager (User) ***[0m
+echo.
+winget install Git.GCM
+echo.
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Installation Git Credential Manager (User) complete!', 'polsoft.ITS London', [System.Windows.Forms.ToolTipIcon]::None)}"
+echo GitHub: https://github.com/seb07uk
+timeout /t 4 >nul
+exit
