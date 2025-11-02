1fe332cbd74bda40658253f59aa6fcfd68841ad6
seb07uk <156347616+seb07uk@users.noreply.github.com>
2025-11-02 05:23:15 +0000

    system tool
    System tools scanning, diagnostics, repair, etc.

---
+++ system tool/Defrag Disk (C).bat
@@ -0,0 +1,32 @@
+:: Sebastian Januchowski
+:: polsotf.ITS London
+:: polsoft.its@fastservice.com
+@echo off
+title Disk (C) Defrag by polsoft.ITS
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+	echo   [3;2mWritten by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com 
+echo. [0m
+	echo.
+    echo                                      [1;3;31mThis script requires administrator privileges.[0m
+    echo                                          [3;1;31mPlease run Script as an administrator.[0m
+	echo.
+	echo                                                [5mPress any key to continue.[0m
+	pause >nul
+    exit
+)
+setlocal EnableExtensions DisableDelayedExpansion
+	echo  [3;2mWritten by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com 
+echo. [0m
+echo Defragmenting hard disks C: ...
+echo.[33m
+ping localhost -n 3 >nul
+defrag c: -v
+echo.[0m
+echo Disk C: defragmentation completed successfully !!!
+echo.
+echo.
+echo Press any key to exit. . .
+echo.
+echo   [44m[3mWritten by Sebastian Januchowski                  polsoft.ITS                   e-mail: polsoft.its@fastservice.com[0m
+pause >nul
